c0fca4ec2f4099d433a76a1935d8fe01fd336a43
hannes.aasamets <hannes.aasamets@bigbank.ee>
2021-07-06 19:02:16 +0300

    Add VueStyleguidist 4.

---
+++ styleguide.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	// set your styleguidist configuration here
+	serverPort: 9000,
+	usageMode: 'expand',
+	title: 'Default Style Guide',
+	components: 'src/components/**/[A-Z]*.vue',
+	defaultExample: true,
+	// sections: [
+	//   {
+	//     name: 'First Section',
+	//     components: 'src/components/**/[A-Z]*.vue'
+	//   }
+	// ],
+	// webpackConfig: {
+	//   // custom config goes here
+	// },
+	exampleMode: 'expand'
+}
